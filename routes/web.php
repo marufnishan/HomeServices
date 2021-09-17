0b9080e3b30bd7b22dd166ca030ef54a248fa533
Maruf Nishan <nasim15-2328@diu.edu.bd>
2021-09-17 19:40:19 +0600

    Search Services Added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SearchController;
 use App\Http\Livewire\Admin\AdminAddServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddServiceComponent;
 use App\Http\Livewire\Admin\AdminAddSlideComponent;
@@ -38,6 +39,9 @@ Route::get('/',HomeComponent::class)->name('home');
 Route::get('/service-categories',ServiceCategoriesComponent::class)->name('home.service_categories');
 Route::get('/{category_slug}/services',ServicesByCategoryComponent::class)->name('home.services_by_category');
 Route::get('/service/{service_slug}',ServiceDetailsComponent::class)->name('home.service_details');
+
+Route::get('/autocomplete',[SearchController::class,'autocomplete'])->name('autocomplete');
+Route::post('/search',[SearchController::class,'searchService'])->name('searchService');
 
 //For Customer
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
