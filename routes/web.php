304bf906f94f1dafb8af5e6acb279256219ab2fe
Maruf Nishan <nasim15-2328@diu.edu.bd>
2021-09-19 21:56:54 +0600

    Service Provider Profile

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Livewire\ServiceCategoriesComponent;
 use App\Http\Livewire\ServiceDetailsComponent;
 use App\Http\Livewire\ServicesByCategoryComponent;
 use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use App\Http\Livewire\Sprovider\SproviderProfileComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -56,6 +57,7 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 //For Service Provider
 Route::middleware(['auth:sanctum', 'verified','authsprovider'])->group(function () {
     Route::get('/sprovider/dashboard',SproviderDashboardComponent::class)->name('sprovider.dashboard');
+    Route::get('/sprovider/profile',SproviderProfileComponent::class)->name('sprovider.profile');
 });
 
 //For Admin
