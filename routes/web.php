5bd7756a7f0a0d544bd17538fd5e7921077cb899
Maruf Nishan <nasim15-2328@diu.edu.bd>
2021-09-19 10:59:47 +0600

    Change Location Added

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Livewire\Admin\AdminServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminServicesByCategoryComponent;
 use App\Http\Livewire\Admin\AdminServicesComponent;
 use App\Http\Livewire\Admin\AdminSliderComponent;
+use App\Http\Livewire\ChangeLocationComponent;
 use App\Http\Livewire\Customer\CustomerDashboardComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ServiceCategoriesComponent;
@@ -42,6 +43,9 @@ Route::get('/service/{service_slug}',ServiceDetailsComponent::class)->name('home
 
 Route::get('/autocomplete',[SearchController::class,'autocomplete'])->name('autocomplete');
 Route::post('/search',[SearchController::class,'searchService'])->name('searchService');
+
+Route::get('/change-location',ChangeLocationComponent::class)->name('home.change_location');
+
 
 //For Customer
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
